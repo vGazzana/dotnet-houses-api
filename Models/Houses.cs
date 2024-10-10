0ce7b0a1184a28893ff6878424b511a6b7f6175a
wsl <vini.gazzana@gmail.com>
2024-10-09 22:39:13 -0300

    feat: Houses model

---
+++ Models/Houses.cs
@@ -0,0 +1,19 @@
+namespace respTest1.Models
+{
+
+	public class Houses
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+
+		public string Founded { get; set; } = string.Empty;
+
+		public string Region { get; set; } = string.Empty;
+
+		public string Founder { get; set; } = string.Empty;
+
+		public string DiedOut { get; set; } = string.Empty;
+	}
+
+}
